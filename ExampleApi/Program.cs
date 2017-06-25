e20c93689bb663d3052e4c1688656fffd5896ab6
jriver27 <713javierrivera@gmail.com>
2017-06-25 15:33:32 -0500

    Found better cmd line Example
    The old project was built with visual studio. However, I was getting strange errors. This project was created using the CLI

--- ExampleApi/Program.cs
+++ ExampleApi/Program.cs
@@ -17,7 +17,6 @@ namespace ExampleApi
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
