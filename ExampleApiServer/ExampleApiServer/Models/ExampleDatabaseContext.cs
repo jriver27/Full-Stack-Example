edde4047897e07abfc27116fa43b1959f6adc2ca
jriver27 <713javierrivera@gmail.com>
2017-06-27 17:43:09 -0500

    Committing database

---
+++ ExampleApiServer/ExampleApiServer/Models/ExampleDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ExampleApiServer.Models
+{
+    public partial class ExampleDatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Initial Catalog=ExampleDatabase;Integrated Security=True;Pooling=False;Connect Timeout=30");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
